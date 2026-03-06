ba861ed37b61e12f08a70531d3c5200b9c022386
Dineshkumar Sekar <dineshkumarskr@gmail.com>
2026-03-06 12:42:41 +0530

    added few more programs for file reading and URL reading in TesterClass.java

--- src/test/java/stepDefinition/TesterClass.java
+++ src/test/java/stepDefinition/TesterClass.java
@@ -5,26 +5,84 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Properties;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.io.*;
+import java.net.*;
 
 public class TesterClass {
 
-	public static void main(String[] args) throws SQLException {
-		String url = "jdbc:sqlserver://localhost:1433;databaseName=MyFirstDB;integratedSecurity=true;encrypt=true;trustServerCertificate=true";
-		Connection conn = DriverManager.getConnection(url);
-		System.out.println("Connected to SQL Server with Windows Authentication!");
+	public static void main(String[] args) throws IOException {
 
-		Statement stmt = conn.createStatement();
-		String SQL = "Select TOP 1 Name from Employees";
-		ResultSet rs = stmt.executeQuery(SQL); //for select query
-		//int affectedrows = stmt.executeUpdate(SQL); //for update query	
+		// ------------------------------Datatabase Connection
+		// String-------------------------------------------
+		/*
+		 * String url =
+		 * "jdbc:sqlserver://localhost:1433;databaseName=MyFirstDB;integratedSecurity=true;encrypt=true;trustServerCertificate=true";
+		 * Connection conn = DriverManager.getConnection(url);
+		 * System.out.println("Connected to SQL Server with Windows Authentication!");
+		 * 
+		 * Statement stmt = conn.createStatement(); String SQL =
+		 * "Select TOP 1 Name from Employees"; ResultSet rs = stmt.executeQuery(SQL);
+		 * //for select query //int affectedrows = stmt.executeUpdate(SQL); //for update
+		 * query
+		 * 
+		 * while (rs.next()) { System.out.println("fetched value from DB is: " +
+		 * rs.getString("Name"));
+		 */
+		// ------------------------------End of Datatabase Connection
+		// String-------------------------------------------
+
+		// ------------------------------Reading a File from URL and handling the string
+		// -------------------------------------------
+		/*
+		 * URL url = new URL("https://temp.staticsave.com/69aa7a3a8ff85.txt");
+		 * BufferedReader br = new BufferedReader(new
+		 * InputStreamReader(url.openStream())); String line;
+		 * System.out.println(br.readLine());
+		 * 
+		 * LocalDateTime now = LocalDateTime.now(); DateTimeFormatter formatter =
+		 * DateTimeFormatter.ofPattern("yyyy-MM-dd"); String date =
+		 * now.format(formatter);
+		 * 
+		 * String date1 = "2026-03-06"; int count=0; try {
+		 * while((line=br.readLine())!=null) { System.out.println(line); String[] parts
+		 * = line.split(" "); if(parts.length>4) { continue; }
+		 * if(parts[0].equals(date1)) { if(parts[2].equals("Block")){ count = count+1; }
+		 * } } System.out.println("The blocked IPs for the given days is: "+count); }
+		 * catch (IOException e) { e.printStackTrace(); }
+		 */
+		// ------------------------------End of Reading a File from URL and handling the
+		// string -------------------------------------------
 		
-		while (rs.next()) { 
-			System.out.println("fetched value from DB is: " + rs.getString("Name")); 
-		}
+		
+		// ------------------------------Reading a File from Local directory and handling the string
+				// -------------------------------------------
+				
+		  
+		/*
+		 * BufferedReader br = new BufferedReader(new
+		 * FileReader("C:\\Work\\Project\\mySeleniumBDDFramework\\target\\iPsText.txt"))
+		 * ; String line; System.out.println(br.readLine());
+		 * 
+		 * LocalDateTime now = LocalDateTime.now(); DateTimeFormatter formatter =
+		 * DateTimeFormatter.ofPattern("yyyy-MM-dd"); String date =
+		 * now.format(formatter); String date1 = "2026-03-06"; int count=0; try {
+		 * while((line=br.readLine())!=null) { System.out.println(line); String[] parts
+		 * = line.split(" "); if(parts.length>4) { continue; }
+		 * if(parts[0].equals(date1)) { if(parts[2].equals("Block")){ count = count+1; }
+		 * } } System.out.println("The blocked IPs for the given days is: "+count); }
+		 * catch (IOException e) { e.printStackTrace(); }
+		 */
 		 
-		
-		//System.out.println("Rows updated: " + affectedrows);
-		
+		// ------------------------------End of Reading a File from Local directory and handling the
+		// string -------------------------------------------
 	}
 
 }
